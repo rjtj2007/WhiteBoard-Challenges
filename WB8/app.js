55174721b3605ca1c1322eaeb7f5679619a8c072
rjtj2007 <teacherman1@gmail.com>
2018-04-28 11:14:15 -0700

    added image for WB8

--- WB8/app.js
+++ WB8/app.js
@@ -9,8 +9,8 @@ function isDiff (obj1, obj2) {
        }
     }
     for (let key in obj2) {
-        let val1 = obj1[key];
-        let val2 = obj2[key];
+        let val1 = obj2[key];
+        let val2 = obj1[key];
         if (val1 !== val2) {
             return true;
         }
